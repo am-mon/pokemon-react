55521835b511e009145b1edb7cc2cd554d1d99bd
Myat Mon <ayemyatmon.yn@gmail.com>
2025-06-29 17:56:24 +0100

    updated searchbox

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,6 +13,7 @@ export default function Header() {
     if (keyword.trim() !== "") {
       navigate(`/search?s=${keyword.trim().toLowerCase()}`);
       setKeyWord("");
+      setSearchOpen(false);
     }
   };
 
